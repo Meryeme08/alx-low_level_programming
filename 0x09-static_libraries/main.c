a70e0a6d1ba873587e6b81112a9c7b3f7464734f
Meryeme <bouchritmeryeme@gmail.com>
2023-04-09 10:15:13 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,13 @@
 #include "main.h"
 
+/**
+ * main - Prints a quote by Kevin Mitnick using the _puts function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker\"
+			""\n\t- Kevin Mitnick");
+	return (0);
 }
