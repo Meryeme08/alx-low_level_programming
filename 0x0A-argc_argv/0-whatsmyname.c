188150043aeaafe61f520a51cb150ea86d6452d1
Meryeme <bouchritmeryeme@gmail.com>
2023-04-10 09:56:00 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	char *program_name = argv[0];
+
+	printf("%s\n", program_name);
+
+	/* Use argc to avoid the warning */
+	(void)argc;
+
+	return (0);
+}
