8f5623af51662551f1cb87541977fef43fcd1b64
Meryeme <bouchritmeryeme@gmail.com>
2023-04-13 18:28:04 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,46 @@
-#include <stdlib.h>
 #include "main.h"
-#include <string.h>
+#include <stdlib.h>
 
 /**
- * *string_nconcat - concatenates n bytes of a string to another string
- * @s1: string to append to
- * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
+ * string_nconcat - Concatenates two strings using at
+ *                  most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
  *
- * Return: pointer to the resulting string
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	size_t s1_len, s2_len, num_bytes;
-	char *result;
+	char *concat;
+	unsigned int len = n, index;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	s1_len = strlen(s1);
-	s2_len = strlen(s2);
+	for (index = 0; s1[index]; index++)
+		len++;
 
-	/* Determine the actual number of bytes to concatenate */
-	num_bytes = (n < s2_len) ? n : s2_len;
+	concat = malloc(sizeof(char) * (len + 1));
 
-	result = malloc(s1_len + num_bytes + 1); /* +1 for the null terminator */
-
-	if (result == NULL)
+	if (concat == NULL)
 		return (NULL);
 
-	/* Copy the first string into the result */
-	strncpy(result, s1, s1_len);
+	len = 0;
 
-	/* Concatenate the second string, up to the specified number of bytes */
-	strncat(result + s1_len, s2, num_bytes);
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
 
-	/* Add the null terminator */
-	result[s1_len + num_bytes] = '\0';
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
 
-	return (result);
+	concat[len] = '\0';
+
+	return (concat);
 }
+
+
