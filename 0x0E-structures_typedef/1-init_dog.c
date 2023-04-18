0aa87e83ef78c07b4285616aefec47617cd660d3
Meryeme <bouchritmeryeme@gmail.com>
2023-04-17 18:26:31 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,10 +11,11 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	struct dog new_dog;
+
 	new_dog.name = name;
 	new_dog.age = age;
 	new_dog.owner = owner;
-	
+
 	if (d != NULL)
 		*d = new_dog;
 }
