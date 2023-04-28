80afbfccd27f23f11051d2649de6962c7f6b5660
Meryeme <bouchritmeryeme@gmail.com>
2023-04-27 17:05:13 -0700

    commit mandatory

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,19 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	const list_t *current_node = h;
-	size_t node_count = 0;
+	size_t s = 0;
 
-	printf("[%u] %s\n", h->len, (h->str != NULL) ? h->str : "(nil)");
+	while (h)
 	{
-		if (current_node->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", current_node->len, current_node->str);
-
-		node_count++;
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
 	}
 
-	return (node_count);
+	return (s);
 }
 
