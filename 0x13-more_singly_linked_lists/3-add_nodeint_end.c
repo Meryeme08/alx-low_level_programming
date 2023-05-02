65f074ea1a0e7f8f52b4042c93039f7b46cad6f5
Meryeme <bouchritmeryeme@gmail.com>
2023-05-02 11:42:39 -0700

    commit mandatory

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
-	
+
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
@@ -27,10 +27,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	/* separate block to declare temp */
 	{
 		listint_t *temp = *head;
+
 		while (temp->next)
 			temp = temp->next;
 	}
-	
+
 	return (new);
 }
 
